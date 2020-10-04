17fc35fb105b9c52928610f2da56d9592a6c6360
adrianromanski <adrianromanski@icloud.com>
2020-10-04 10:45:41 +0200

    Added Query For Github Api

--- src/main/java/pl/training/search/Application.java
+++ src/main/java/pl/training/search/Application.java
@@ -6,6 +6,7 @@ import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import pl.training.search.github.GithubService;
+import pl.training.search.github.Repository;
 import pl.training.search.wikipedia.Article;
 import pl.training.search.wikipedia.WikipediaService;
 import retrofit2.Retrofit;
@@ -14,6 +15,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Application {
 
@@ -38,10 +40,27 @@ public class Application {
         return newResult;
     }
 
+    private List<String> combineResults(List<String> results, List<String> otherResults) {
+        List<String> newResults = new ArrayList<>();
+        newResults.addAll(otherResults);
+        newResults.addAll(results);
+        return newResults;
+    }
+
     private Observable<List<String>> sendWikipediaQuery(String query) {
         return wikipediaService.getArticles(query)
                 .flatMap(Observable::fromIterable)
                 .map(Article::getTitle)
+                .reduce(new ArrayList<>(), this::combine)
+                .toObservable()
+                .subscribeOn(Schedulers.io());
+    }
+
+
+    private Observable<List<String>> sendGithubQuery(String query) {
+        return githubService.getRepositories(query)
+                .flatMap(Observable::fromIterable)
+                .map(Repository::getName)
                 .reduce(new ArrayList<>(), this::combine)
                 .toObservable()
                 .subscribeOn(Schedulers.io());
@@ -55,10 +74,18 @@ public class Application {
     private void start() {
         Runtime.getRuntime()
                 .addShutdownHook(new Thread(compositeDisposable::dispose));
+//        compositeDisposable.add(
+//                ObservableReader.from(System.in)
+//                    .flatMap(this::sendWikipediaQuery)
+//                    .subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"))
+//        );
+
         compositeDisposable.add(
                 ObservableReader.from(System.in)
-                    .flatMap(this::sendWikipediaQuery)
-                    .subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"))
+                .flatMap(query -> Observable.zip(sendWikipediaQuery(query), sendGithubQuery(query), this::combineResults))
+                .flatMap(Observable::fromIterable)
+                .map(String::toLowerCase)
+                .subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"))
         );
     }
 }
